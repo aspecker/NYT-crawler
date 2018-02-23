3888828519fe4529c6e8c91d162b864cb8c6eb02
Adam Specker <specker@gmail.com>
2018-02-23 16:40:52 -0500

    working scraper with some styling. final revisions needed

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,19 +1,47 @@
 
-$(insert button here).on("click", function(){
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-    queryURL += '?' + $.param({
-        'api-key': "26f30e7b925b498680e5359d7c5627a5"
-    });
+$("#submitBtn").on("click", function(event){
+    event.preventDefault();
+    $("#articleBox").empty();
+    
+    // take input from forms
+    var searchTerm = $("#searchTerm").val();
+    var limit = $("#resultLength").val();
+    var start = $("#startYear").val();
+    var end = $("#endYear").val();
+
+    // Urls for different situations
+    var url = "";
+    var urlBoth = "https://api.nytimes.com/svc/search/v2/articlesearch.json?fq="
+    +searchTerm+"&begin_date="+start+"0101&end_date="+end+"0101&api-key=4f3b87ebf5b24f85ab9eba9ab5f59cc1";
+    var urlStart= "https://api.nytimes.com/svc/search/v2/articlesearch.json?fq="
+    +searchTerm+"&begin_date="+start+"0101&api-key=4f3b87ebf5b24f85ab9eba9ab5f59cc1";
+    var urlEnd= "https://api.nytimes.com/svc/search/v2/articlesearch.json?fq="
+    +searchTerm+"&end_date="+end+"0101&api-key=4f3b87ebf5b24f85ab9eba9ab5f59cc1";
+    var urlNeither= "https://api.nytimes.com/svc/search/v2/articlesearch.json?fq="
+    +searchTerm+"&api-key=4f3b87ebf5b24f85ab9eba9ab5f59cc1";
+    
+    // conditional to prevent error if one or both years aren't input
+    if (start===""&&end===""){
+        url = urlNeither;
+    } else if (start===""){
+        url = urlEnd;
+    } else if (end===""){
+        url = urlStart;
+    } else {
+        url = urlBoth;
+    }
     $.ajax({
         url: url,
         method: 'GET',
     }).then(function(response) {
-        console.log(response);
-    }).fail(function(err) {
-        throw err;
+        // after promise, retreive information and append it to the article box div
+        for (var i=0;i<limit;i++){
+        var title = $("<h3>").text(response.response.docs[i].headline.main);
+        var articleURL = $("<a>").attr("href", response.response.docs[i].web_url).append(title);
+        var snippet = $("<p>").text(response.response.docs[i].snippet);
+        var pubDate = $("<p>").html("<b>Published in "+response.response.docs[i].pub_date.slice(0,4)+"</b>");
+        var lineBreak = $("<div>").addClass("lineBreak");
+        $("#articleBox").append(articleURL,snippet,pubDate,lineBreak);
+        }
     });
-
-
-
-    
 });
